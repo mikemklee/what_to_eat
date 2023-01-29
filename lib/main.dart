81289bbb538b09674a3b8da509a89e1a1e95defb
Mike Lee <mikemunkyu.lee@gmail.com>
2023-01-29 17:11:47 -0500

    Add logic to select random item and display its image

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
@@ -27,15 +28,6 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -43,65 +35,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List _items = [];
+  var _item;
 
   Future<void> readJson() async {
     final String response = await rootBundle.loadString('assets/sample.json');
     final data = await json.decode(response);
     setState(() {
-      _items = data['items'];
+      var index = Random().nextInt(data['items'].length);
+      _item = data['items'][index];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('What to eat?', style: TextStyle(fontSize: 20)),
-            _items.isNotEmpty
-                ? Expanded(
-                    child: ListView.builder(
-                      itemCount: _items.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          key: ValueKey(_items[index]["id"]),
-                          margin: const EdgeInsets.all(10),
-                          color: Colors.amber.shade100,
-                          child: ListTile(
-                            leading: Text(_items[index]["id"]),
-                            title: Text(_items[index]["name"]),
-                            subtitle: Text(_items[index]["description"]),
-                          ),
-                        );
-                      },
-                    ),
-                  )
-                : Container(),
+            SizedBox(
+              height: 250,
+              width: 250,
+              child: _item != null
+                  ? Image(
+                      height: 250,
+                      width: 250,
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        _item["imageUrl"],
+                      ),
+                    )
+                  : const Placeholder(),
+            ),
             const SizedBox(height: 10),
             FilledButton(onPressed: readJson, child: const Text('Press me')),
           ],
